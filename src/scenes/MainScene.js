b97203296549fd03572185c64997356df342be98
AyyyCn <adamldr3@gmail.com>
2025-04-06 00:11:47 +0100

    text

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -138,7 +138,7 @@ export class MainScene {
     const box = document.getElementById('announcement-box');
     box.textContent = `👋 Welcome to my 3D Portfolio! \n
     I'm Adam Ladhari, a creative developer passionate about gameDev, AI, and immersive tech.\n
-    💡 Click on labels like "Projects" or "Skills" to explore. You can also move freely or reset the view anytime with the camera button.`;
+    💡 Click on labels like "Projects" or "Skills" to explore. You can also move freely or use the navbar`;
     
     box.style.opacity = 1;
 
