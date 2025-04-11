d47567c8bc43d67f2008e855559fdc3d6f47581a
Adam Ladhari <72625542+AyyyCn@users.noreply.github.com>
2025-04-11 21:25:09 +0100

    Update links.js linkedin

--- src/links.js
+++ src/links.js
@@ -2,7 +2,7 @@ export const links = {
     youtube: 'https://www.youtube.com/@acn8713',
     github: 'https://github.com/AyyyCn',
     facebook: 'https://facebook.com/adam.ladharii',
-    linkedin: 'https://www.linkedin.com/in/adam-ladhari-0815b8276',
+    linkedin: 'https://www.linkedin.com/in/adam-ladhari',
     mail: 'mailto:adamldr3@gmail.com',
     artstation: 'https://www.artstation.com/ayyycn',
   };
